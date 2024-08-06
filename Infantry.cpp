ea782b77b5a9db9800c82ac658a216d03ae1852b
MissNcube <aundreancu@gmail.com>
2024-08-06 17:30:52 +0200

    i have added meat to the functions under the Infantry.cpp

--- Infantry.cpp
+++ Infantry.cpp
@@ -14,14 +14,39 @@ void Infantry::prepare()
 }
 void Infantry::execute()
 {
-
+    cout << getName() << "charges towards the enemy!!" << endl;
+    cout << "The infantry charge at the enemy with spears raised high. " << endl;
+    int battleCounter = (getDamage() > getDefence()) ? 0:2;
+    if(battleCounter == 0)
+    {
+        cout << getName() << " wins the battle! Enemies are retreating..." << endl;
+    }
+    else if(battleCounter == 1)
+    {
+        cout << "It's not looking good for any one of the sides... " << endl;
+    }
+    else{
+        cout << getName() << " is losing this one badly. " << endl;
+    }
 }
 void Infantry::retreat()
 {
+    cout << getName() << " retreats to safer grounds. " << endl;
+    int safe = (getDefence() > getDamage()) ? 0:1;
+    if(safe == 0)
+    {
+        cout << getName() << " has successfully retreated away from enemy forces. " << endl;
+
+    }
+    else{
+        cout << "Enemy troops pursue faster..." << endl;
+    }
 
 }
 void Infantry::rest()
 {
+    int recovBoost = (getHealth() + getAmountOfSoldiers())/10;
+    cout << getName() << " is resting, +" << recovBoost << " health to the infantry " << endl; 
 
 }
 
